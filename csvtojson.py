0b2e0bffcd1058e6332cf234aa2373cee1b8b63d
Pratapagir <anu.pratapagiri@gmail.com>
2024-05-03 11:28:29 +0200

    test

--- csvtojson.py
+++ csvtojson.py
@@ -7,11 +7,13 @@ json_file = 'data.json'
 data = []
 
 #Read the CSV file
-with open(csv_file, 'r') as file:
-    reader = csv.DictReader(file)
-    for row in reader:
+with open(csv_file, 'r', encoding='utf-8-sig') as file:  # Specify encoding
+    csv_data = csv.DictReader(file)
+    for row in csv_data:
         data.append(row)
 
 #Write the JSON file
-with open(json_file, 'w') as file:
-    json.dump(data, file, indent=4)
+with open(json_file, 'w', encoding='utf-8') as file:  # Specify encoding
+    json.dump(data, file, indent=4, ensure_ascii=False)  # Ensure ASCII is False
+
+print("CSV to JSON conversion completed!")
